92dd4ab2f84a9a5ab5dad88b241332b7905718d2
Sully <praisethesun.2017@gmail.com>
2022-09-17 14:01:48 +0100

    updated this commit message with a better desc when back

--- cpu.py
+++ cpu.py
@@ -24,7 +24,44 @@ class Cpu:
         self.PC = 0
         """memory is a list"""
         self.memory = 0
+
+    #The game will be loaded into memory, so retrieve the next byte
+    def fetch_byte(self):
+        byte = self.memory[self.PC]
+        self.PC += 1
+        return byte
+    
+    def fetch_two_bytes(self):
+        high_byte = self.memory[self.PC]
+        low_byte = self.memory[self.PC+1]
+        self.PC += 2 
+        data = high_byte << 8 | low_byte
+        return data
+
+    #MEMORY IS NOT RESET FOR TESTING PURPOSES
+    def test_reset(self):
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.D = 0
+        self.E = 0
+        self.H = 0 
+        self.L = 0 
+        self.BC = 0
+        self.DE = 0
+        self.HL = 0
+        self.SP = 0
+        self.Z = False
+        self.S = False
+        self.P = False
+        self.CY = False
+        self.AC = False
+        self.PC = 0
+
+    
 #--------------------------------------------------#
+
+# testing
 cpu = Cpu()
 # step 1. load spaceinvaders into memory
 with open("invaders", "rb") as b_file:
@@ -32,13 +69,25 @@ with open("invaders", "rb") as b_file:
 
 assert cpu.memory[0] == 0x00
 assert cpu.memory[9] == 0xc5
-assert cpu.memory[len(data)-1] == 0x00
+assert cpu.memory[len(cpu.memory)-1] == 0x00
+
+data = cpu.fetch_byte()
+assert data == 0x00
+
+cpu.test_reset()
+
+data = cpu.fetch_two_bytes()
+assert data == 0x00
+data = cpu.fetch_two_bytes()
+assert data == 0x00
+data = cpu.fetch_two_bytes()
+assert data == 0x00
+data = cpu.fetch_two_bytes()
+assert data == 0x00
+
+
 
 #--------------------------------------------------#
-
-#The game will be loaded into memory, so retrieve the next two bytes from the ROM?#
-def fetch_byte(self):
-    return #incomplete function
 
 #set register BC within this function
 def set_register_BC(self, high_byte, low_byte):
