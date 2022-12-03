60a00b834f695cb46ccb4974a0a276de2baf3ea2
SullyK <68860309+SullyK@users.noreply.github.com>
2022-12-03 00:18:49 +0000

    leaving DAA for now :/
    From here on, I will work on instructions pertraining to the space invaders ROM as I am implementing many unused instructions.

--- cpu.py
+++ cpu.py
@@ -37,7 +37,7 @@ class Cpu:
         self.PC += 1
         return byte
     
-    def fetch_two_bytes(self):
+    def fetch_two_bytes(self): #@@@TODO: RENAME this to better reflect 2 next 2 8 bit bytes to 16 bit
         low_byte = self.memory[self.PC] # The CPU is low endian so we start with the low byte
         high_byte = self.memory[self.PC+1]
         self.PC += 2 
@@ -116,48 +116,79 @@ class Cpu:
     def MVI_B_d8(self):
         self.B = self.fetch_byte()
 
-    def get_MSB(self,A):
-        bits = (A & (1 << 7))  #1 << 7 is the same as 1000 0000
+    def get_MSB(self):
+        bits = (self.A & (1 << 7))  #1 << 7 is the same as 1000 0000
         return ((bits >> 7))
 
-    def get_LSB(self,A):
-        bit = (A & 1)
+    def get_LSB(self):
+        bit = (self.A & 1)
         return bit
 
-    def RLC(self,A):
+    def RLC(self):
         self.CY = self.get_MSB(A) # store the MSB
-        A = (( A << 1) & 0xFF) #shift Left
+        self.A = ((self.A << 1) & 0xFF) #shift Left
         # if CY == 1:
             # A = A | 0b0000001 -- not necessary below is nicer and same
-        A = A | self.CY #if CY is 1, then 1 appended, if 0, nothing happens
+        self.A = self.A | self.CY #if CY is 1, then 1 appended, if 0, nothing happens
         
-    def RAL(self,A):
+    def RAL(self):
         LSB = self.CY
         self.CY = self.get_MSB(A)
-        A = ((A << 1) & 0xFF) # shifted left
-        A = A | LSB
+        self.A = ((self.A << 1) & 0xFF) # shifted left
+        self.A = self.A| LSB
     
-    def RRC(self,A):
+    def RRC(self):
         self.CY = self.get_LSB(A)
-        A = ((A >> 1) & 0xFF)
-        A = A | (self.CY << 7)
+        self.A = ((self.A >> 1) & 0xFF)
+        self.A = self.A | (self.CY << 7)
         
-    def RAR(self,A):
+    def RAR(self):
         MSB = self.CY
-        self.CY = self.get_LSB(A)
-        A = ((A >> 1) & 0xFF)
-        A = A | (MSB << 7)
+        self.CY = self.get_LSB(self.A)
+        self.A = ((self.A >> 1) & 0xFF)
+        self.A = self.A | (MSB << 7)
 
     def DAA(self):
         value = self.A & 15
         if value > 9 or self.AC == True:
-            self.AC += 6
+            self.A += 6
         
         mask = 0b11110000
         value = (self.A & mask)
         value = value >> 4 #shift the back to the bottom 4 bits
         if value > 9 or self.CY == 1:
-            self.A += (6 << 4) #continue from here                
+            self.A += (6 << 4) 
+        #COME BACK AND FIX THESE FLAGS....
+
+    #--------------------------------------------------#
+    
+    # Branch Group
+    def JMP(self):
+        self.PC = self.fetch_two_bytes
+        return
+    
+    
+    #--------------------------------------------------#
+    # Stack, I/O, and Machine Control Group:
+
+ #@@@TODO: some are 1, some True... I need to deal with this
+ # function might not actually be working otherwise
+    def PUSH_PSW(self):
+        self.memory[self.SP - 1] = self.A
+        flag_word = 0
+        flag_word += self.CY << 0 | 1 << 1 | self.P << 2 | self.AC << 4 | self.Z << 6 | self.S << 7
+        self.memory[self.SP - 2] = flag_word
+        self.SP -= 2
+        return
+
+    def PUSH_B(self):
+        mask = 0xFF #255 same as 0b0000000011111111 same as 0b11111111
+        high = self.BC & mask
+        low = self.BC >> 7
+        self.memory[self.SP - 1] = high
+        self.memory[self.SP - 2] = low
+        self.SP -= 2
+        return  
 
     #--------------------------------------------------#
 
@@ -197,6 +228,10 @@ class Cpu:
             case 0x04:
                 self.INR_B()
                 return
+            case 0xc3:
+                self.JMP()
+            case 0xf5:
+                self.PUSH_PSW()
 
 #--------------------------------------------------#
 
